8ef46b5a037484be94eb9aa76acabd161f405000
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2020-05-30 00:55:16 -0300

    feat: add create appointment service

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { parseISO, startOfHour } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '@repositories/appointments.repository';
+import CreateAppointmentService from '@services/appointments/create.service';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -13,23 +14,25 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
-  const appointmentBooked = appointmentsRepository.findByDate(parsedDate);
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository
+    );
 
-  if (appointmentBooked) {
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate
+    });
+
+    return response.json(appointment);
+  } catch (err) {
     return response.status(400).json({
-      message: 'This appointment is already booked'
+      error: err.message
     });
   }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
