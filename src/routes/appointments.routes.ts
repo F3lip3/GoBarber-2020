c6110ddb26104236dfe1bc36ec992dce0b96381d
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2020-05-29 23:47:33 -0300

    feat: add appointments validations

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,34 @@
 import { Router } from 'express';
+import { isEqual, parseISO, startOfHour } from 'date-fns';
 import { uuid } from 'uuidv4';
 
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
 const appointmentsRouter = Router();
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const appointmentBooked = appointments.some(appointment =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (appointmentBooked) {
+    return response.status(400).json({
+      message: 'This appointment is already booked'
+    });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date
+    date: parsedDate
   };
 
   appointments.push(appointment);
