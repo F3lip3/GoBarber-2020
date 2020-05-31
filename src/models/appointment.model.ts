17e1546b8992cf98de6c0c59573711d038fb23c4
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2020-05-31 01:09:26 -0300

    feat: add provider id link to appointments

--- src/models/appointment.model.ts
+++ src/models/appointment.model.ts
@@ -3,8 +3,11 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn
 } from 'typeorm';
+import User from '@models/user.model';
 
 @Entity('appointments')
 class Appointment {
@@ -12,7 +15,11 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('time with time zone')
   date: Date;
