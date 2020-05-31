627130406b6c34c61c6555eb05dc52ebf38c9414
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2020-05-31 00:34:16 -0300

    feat: add typeorm to save appointments

--- src/services/appointments/create.service.ts
+++ src/services/appointments/create.service.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '@models/appointment.model';
 import AppointmentsRepository from '@repositories/appointments.repository';
@@ -9,15 +10,11 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
-    const appointmentBooked = this.appointmentsRepository.findByDate(
+    const appointmentBooked = await appointmentsRepository.findByDate(
       appointmentDate
     );
 
@@ -25,10 +22,12 @@ class CreateAppointmentService {
       throw Error('This appointment is already booked');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
